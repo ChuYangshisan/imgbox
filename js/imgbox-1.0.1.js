1d01462463c2a43d32b58e2fc13d5903cebfce43
Nisus-Liu <609069481@qq.com>
2018-07-25 12:37:38 +0800

    feat: 增加旋转功能(subject to this)

--- js/imgbox-1.0.1.js
+++ js/imgbox-1.0.1.js
@@ -14,7 +14,7 @@
  *   1. 标签选择器调用, 1. $('xx').imgbox() or 2. $('xx').imgbox(settings);
  *   2. ImgBox.imgbox(img [,settings])包装img选择器或Image对象
  *
- * TODO 滚动放大缩小时, 图片位置会跳到中间, 不论当前图片被拖动到了哪里
+ *
  *
  * -v1.0.1-
  * 2018-7-23 13:14:35
@@ -51,29 +51,49 @@
         //height:
     };
 
-
-    //全局对象, 暴露给外部使用的
-    var ImgBox = window.ImgBox = {
-        origins: [],
-        //当前被激活的Origin对象, i.e.其大图被处于预览状态
-        activeOrigin: null,
+    function ImgBox() {
+        this.origins = [],
+            //当前被激活的Origin对象, i.e.其大图被处于预览状态
+            this.activeOrigin = null;
         //大图片显示框
         // this.$zoomArea = $('<div class="__max_div__"><div class="__close_button__" title="点击关闭" onclick="hideMax()"><img src="img/delete.png"></div></div>');
         //存放放大图片的区域容器
-        $zoomArea: $('<div class="imgbox-area" style="display: none;"></div>'),     //TODO 增加关闭按钮
-        //遮罩层
-        $cover: $('<div class="imgbox-cover" style="display: none;"></div>'),
+        this.$zoomArea = $('<div class="imgbox-area" style="display: none;"></div>');
+        this.$toolbarDom = $('<div class="imgbox-toolbar"><ul>' +
+            '<li class="btn-rotate rotate-left"></li>' +
+            '<li class="btn-rotate rotate-right"></li></ul></div>');
+        this.$titleDom = $('<div class="imgbox-item-title"></div>');
 
-    };
+
+        this.$zoomArea.append(this.$titleDom).append(this.$toolbarDom);
+
+        //遮罩层
+        this.$cover = $('<div class="imgbox-cover" style="display: none;"></div>');
+
+        //定义旋转方法
+        this.rotate = function (e) {
+            //调用Origin自己的rotate方法
+            this.activeOrigin.rotate(e.data);
+        }.bind(this);
+
+    }
+
+    //全局对象, 暴露给外部使用的
+    var ImgBox = window.ImgBox = new ImgBox();
 
 
     ImgBox.$cover.bind('click', function () {
         ImgBox.hideMax();
     });
 
+
     //将大图容器和遮罩层添加至body中
     $(document).ready(function () {
         $('body').append(ImgBox.$cover).append(ImgBox.$zoomArea);
+        //绑定事件
+        ImgBox.$zoomArea.find('.rotate-left').bind('click', 'left', ImgBox.rotate);
+        ImgBox.$zoomArea.find('.rotate-right').bind('click', 'right', ImgBox.rotate);
+
     });
 
 
@@ -92,6 +112,11 @@
 
         //大图片box, 拖拽, 放大缩小, 其他按钮(其他按钮若是公用的则放到zoomArea中, 私有的则放到这里)
         this.$largeBox = $('<div class="imgbox-item"></div>');
+        //预图片项的标题
+        this.title = this.$self.attr('title');
+        if (!this.title) this.title = '';
+        //this.$title = $('<div class="imgbox-item-title">' + title + '</div>');
+        this.$largeBox.append(this.$title);
         this.largeImage = new Image();
         this.$largeBox.append(this.largeImage);
         if (this.$self.attr('large-src')) {
@@ -103,6 +128,91 @@
 
         //大图配置
         this.configLargeBox();
+
+        this.degree = 0;
+        this.rotate = function (direction) {
+            if (direction == 'left') {
+                this.degree -= 90;
+            }
+            if (direction == 'right') {
+                this.degree += 90;
+            }
+            //样式
+            this.$largeBox.css('transform', 'rotate(' + this.degree + 'deg)');
+
+        };
+        //判断当前坐标轴是否应该翻转: 图片旋转造成定位时宽高应该适当翻转
+        this.isInverseAxis = function () {
+            return Math.abs(this.degree) / 90 % 2 != 0;
+        };
+        //清除旋转 ==> 角度置为0, 样式置为0deg
+        this.cleanRotate = function () {
+            this.degree = 0;
+            this.rotate();
+        }
+
+        //配置大图box
+        this.configLargeBox = function () {
+            var _this = this;
+
+            //大图url没有设置, 则沿用小图url
+            var hasMaxUrl = true;  //是否有大图链接
+            if (_this.largeImage.src == null || _this.largeImage.src == '') {
+                _this.largeImage.src = _this.smallImage.src;
+                hasMaxUrl = false;
+            }
+
+            $(_this.largeImage).addClass("imgbox-large-image");
+
+            //宽高处理  没有定制则显示原始尺寸
+            //有大图链接, 则以大图尺寸为基准, 没有则以小图为基准
+            if (_this.options.width != null) {      //有限使用指定width
+                //大图的初始width, 下文缩放以此为基准
+                _this.largeImage.baseWidth = _this.options.width * _this.options.magnification;
+                $(_this.largeImage).css({'width': _this.largeImage.baseWidth});
+            } else if (!hasMaxUrl) {                //没有指定width, 也没有大图, 则显示缩略图原始尺寸
+                _this.largeImage.baseWidth = _this.smallImage.naturalWidth * _this.options.magnification;   //若要以小图的css样式则用width取值
+                //获取真实尺寸, 然后放大
+                $(_this.largeImage).css({'width': _this.largeImage.baseWidth});        //否则在原尺寸扩大的基础上扩大
+            } else {
+                //没有指定宽度, 但有大图链接, 则使用大图的原始尺寸, i.e. 图片真实尺寸
+                _this.largeImage.baseWidth = _this.largeImage.naturalWidth * _this.options.magnification;
+                $(_this.largeImage).width(_this.largeImage.baseWidth);
+            }
+            if (_this.options.height != null) {
+                _this.largeImage.baseHeight = _this.options.height * _this.options.magnification;
+                $(_this.largeImage).css({"height": _this.largeImage.baseHeight});
+            } else if (!hasMaxUrl) {
+                _this.largeImage.baseHeight = _this.smallImage.naturalHeight * _this.options.magnification;
+                $(_this.largeImage).css({"height": _this.largeImage.baseHeight});
+            } else {
+                _this.largeImage.baseHeight = _this.largeImage.naturalHeight * _this.options.magnification;
+                $(_this.largeImage).height(_this.largeImage.baseHeight);
+            }
+
+            //清除旋转
+            _this.cleanRotate();
+
+            //配置鼠标事件
+            _this.largeImage.onmouseover = function () {
+                _enter = true;
+            };
+            _this.largeImage.onmouseout = function () {
+                _enter = false;
+            };
+
+
+            //居中显示
+            //大图居中
+            _centerize(_this);
+
+
+            //拖拽事件
+            _dragFunc(_this.$largeBox);
+
+
+            return _this.$largeBox;
+        };
 
     };
 
@@ -178,120 +288,83 @@
     };
 
 
-    function _centerize(box, img) {
-        $(box).css({
+    function _centerize(origin) {
+        var $box = origin.$largeBox;
+        var $img = $(origin.largeImage);
+        var ow, oh;
+        ow = $img.outerWidth();
+        oh = $img.outerHeight();
+
+
+        $box.css({
             position: "absolute",
             // 'margin-left': -box.width / 2,                  //图片宽度的一般(负值)    ($(window).width() - $(_this.largeImage).outerWidth()) / 2,
             // 'margin-top': -box.height / 2,                   //($(window).height() - $(_this.largeImage).outerHeight()) / 2
             // top: '50%',
             // left: '50%'
-            left: ($(window).width() - $(img).outerWidth()) / 2,
-            top: ($(window).height() - $(img).outerHeight()) / 2
+            left: ($(window).width() - ow) / 2,
+            top: ($(window).height() - oh) / 2
         });
     }
 
-    /**由区域中心向四周放大, 或向中心收缩*/
-    function _radiusScroll(box, size) {  //size:[oldWidth,oldHeight,newWight,newHeight]
-        var left = box.position().left - (size[2] - size[0]) / 2;       //position().left 获取的是数值
-        var top = box.position().top - (size[3] - size[1]) / 2;
-        $(box).css({
-            left: left,
-            top: top
+    /**由区域中心向四周放大, 或向中心收缩
+     * Note: 浏览器会自动帮我们处理旋转之后的宽高差*/
+    function _radiusScroll(origin, size) {  //size:[oldWidth,oldHeight,newWight,newHeight]
+        console.log(size);
+        console.log('old w/h', size[0] / size[1]);
+        console.log('new w/h', size[2] / size[3]);
+        var $box = origin.$largeBox;
+        var dleft, dtop;
+        var oldleft_css = parseInt($box.css('left'));
+        var oldtop_css = parseInt($box.css('top'));
+
+        //不用自己处理翻转坐标后的宽高差
+        dleft = (size[2] - size[0]) / 2;
+        dtop = (size[3] - size[1]) / 2;
+        var left = oldleft_css - dleft;       //position().left 获取的是数值
+        var top = oldtop_css - dtop;
+
+        console.log('new left', left);
+        console.log('new top', top);
+        $box.css({
+            left: left + 'px',
+            top: top + 'px'
         });
     }
 
 
-    Origin.prototype.configLargeBox = function () {
-        var _this = this;
+    Origin.prototype.configLargeBox =
+        ImgBox.showMax = function (origin) {
+            var _this = this;
+            if (origin == null) return;
+            //判断是否更换了origin
+            if (_this.activeOrigin != origin) {
+                //移除旧的
+                //$(origin.largeImage).remove();
+                //$(origin.$zoomArea).remove();
+                if (_this.activeOrigin != null) _this.activeOrigin.$largeBox.remove();
 
-        //大图url没有设置, 则沿用小图url
-        var hasMaxUrl = true;  //是否有大图链接
-        if (_this.largeImage.src == null || _this.largeImage.src == '') {
-            _this.largeImage.src = _this.smallImage.src;
-            hasMaxUrl = false;
-        }
+                //更新大图对象的配置     Mod: 每个Origin的大图有它自己的配置信息, 并且切换Origin后, 依然保留在
+                origin.configLargeBox();
 
-        $(_this.largeImage).addClass("imgbox-large-image");
+                //更新大图div中image
+                _this.$zoomArea.append(origin.$largeBox);
+                //更新标题
+                _this.$titleDom.text(origin.title);
+                //甩开图片(图片们有更新, 才需要执行)
+                ImgBox.throwOffImg();
+            }
 
-        //宽高处理  没有定制则显示原始尺寸
-        //有大图链接, 则以大图尺寸为基准, 没有则以小图为基准
-        if (_this.options.width != null) {      //有限使用指定width
-            //大图的初始width, 下文缩放以此为基准
-            _this.largeImage.baseWidth = _this.options.width * _this.options.magnification;
-            $(_this.largeImage).css({'width': _this.largeImage.baseWidth});
-        } else if (!hasMaxUrl) {                //没有指定width, 也没有大图, 则显示缩略图原始尺寸
-            _this.largeImage.baseWidth = _this.smallImage.naturalWidth * _this.options.magnification;   //若要以小图的css样式则用width取值
-            //获取真实尺寸, 然后放大
-            $(_this.largeImage).css({'width': _this.largeImage.baseWidth});        //否则在原尺寸扩大的基础上扩大
-        } else {
-            //没有指定宽度, 但有大图链接, 则使用大图的原始尺寸, i.e. 图片真实尺寸
-            _this.largeImage.baseWidth = _this.largeImage.naturalWidth * _this.options.magnification;
-            $(_this.largeImage).width(_this.largeImage.baseWidth);
-        }
-        if (_this.options.height != null) {
-            _this.largeImage.baseHeight = _this.options.height * _this.options.magnification;
-            $(_this.largeImage).css({"height": _this.largeImage.baseHeight});
-        } else if (!hasMaxUrl) {
-            _this.largeImage.baseHeight = _this.smallImage.naturalHeight * _this.options.magnification;
-            $(_this.largeImage).css({"height": _this.largeImage.baseHeight});
-        } else {
-            _this.largeImage.baseHeight = _this.largeImage.naturalHeight * _this.options.magnification;
-            $(_this.largeImage).height(_this.largeImage.baseHeight);
-        }
+            //显示遮罩层
+            _this.$cover.show();
+            //document.body.appendChild(_this.$cover[0]);
+            //显示maxDiv
+            _this.$zoomArea.show();
 
-        //配置鼠标事件
-        _this.largeImage.onmouseover = function () {
-            _enter = true;
+            //更新当前正在显示大图的 origin
+            _this.activeOrigin = origin;
+
         };
-        _this.largeImage.onmouseout = function () {
-            _enter = false;
-        };
-
-
-        //居中显示
-        //大图居中
-        _centerize(_this.$largeBox, _this.largeImage);
-
-
-        //拖拽事件
-        _dragFunc(_this.$largeBox);
-
-
-        return _this.$largeBox;
-    };
-
-
-    /*放大原始图片  i.e. 加载imgbox-max标签指定的大图
-    * 若大图url==小图url -> 不重新加载, 接着用已有的*/
-    ImgBox.showMax = function (origin) {
-        var _this = this;
-        //判断是否更换了origin
-        if (_this.activeOrigin != origin) {
-            //移除旧的
-            //$(origin.largeImage).remove();
-            //$(origin.$zoomArea).remove();
-            _this.$zoomArea.empty();
-
-            //更新大图对象的配置     Mod: 每个Origin的大图有它自己的配置信息, 并且切换Origin后, 依然保留在
-            origin.configLargeBox();
-
-            //更新大图div中image
-            _this.$zoomArea.append(origin.$largeBox);
-
-            //甩开图片(图片们有更新, 才需要执行)
-            ImgBox.throwOffImg();
-        }
-
-        //显示遮罩层
-        _this.$cover.show();
-        //document.body.appendChild(_this.$cover[0]);
-        //显示maxDiv
-        _this.$zoomArea.show();
-
-        //更新当前正在显示大图的 origin
-        _this.activeOrigin = origin;
-
-    };
 
     //!解决鼠标松开后, 图片还跟着鼠标走!
     //禁止拖动页面图片在新窗口打开
@@ -349,7 +422,7 @@
         //var aspectRatio = h / w;
 
 
-        function zoomOut(origin, w,h,r) {
+        function zoomOut(origin, w, h, r) {
             if (r >= origin.options.max) return;
             //每滚一下增加10/%        保持纵横比
             $(origin.largeImage).css({
@@ -357,8 +430,9 @@
                 "height": (h * 1.1) + "px"
             });
         }
-        function zoomIn(origin,w,h, r) {
-            if(r <= origin.options.min) return;
+
+        function zoomIn(origin, w, h, r) {
+            if (r <= origin.options.min) return;
             $(origin.largeImage).css({
                 "width": (w * 0.9) + "px",
                 "height": (h * 0.9) + "px"
@@ -367,7 +441,7 @@
 
         if (e.wheelDelta && _enter) {  //判断浏览器IE，谷歌滑轮事件
             if (e.wheelDelta > 0) { //当滑轮向上滚动时
-                zoomOut(ImgBox.activeOrigin,w,h,r);
+                zoomOut(ImgBox.activeOrigin, w, h, r);
 
                 // console.log('naturalWidth',ImgBox.activeOrigin.largeImage.naturalWidth);
                 // console.log('naturalHeight',ImgBox.activeOrigin.largeImage.naturalHeight);
@@ -377,20 +451,20 @@
                 // ImgBox.$zoomArea.find('.__close_button__').css("margin-left",(w-125)<125?125:(w-125)+"px");
             }
             if (e.wheelDelta < 0) { //当滑轮向下滚动时
-                zoomIn(ImgBox.activeOrigin,w,h, r);
+                zoomIn(ImgBox.activeOrigin, w, h, r);
             }
         } else if (e.detail && _enter) {  //Firefox滑轮事件
             if (e.detail > 0) { //当滑轮向下滚动时
-                zoomIn(ImgBox.activeOrigin, w,h,r);
+                zoomIn(ImgBox.activeOrigin, w, h, r);
             }
             if (e.detail < 0) { //当滑轮向上滚动时
-                zoomOut(ImgBox.activeOrigin, w,h,r);
+                zoomOut(ImgBox.activeOrigin, w, h, r);
             }
         }
 
         //居中显示
         //大图居中
-        _radiusScroll(ImgBox.activeOrigin.$largeBox, [w, h, ImgBox.activeOrigin.largeImage.width, ImgBox.activeOrigin.largeImage.height]);
+        _radiusScroll(ImgBox.activeOrigin, [w, h, ImgBox.activeOrigin.largeImage.width, ImgBox.activeOrigin.largeImage.height]);
 
 
     }; //给页面绑定滑轮滚动事件
